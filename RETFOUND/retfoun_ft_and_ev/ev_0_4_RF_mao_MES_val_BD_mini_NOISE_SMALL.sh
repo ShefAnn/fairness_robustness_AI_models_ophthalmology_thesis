0084edc3f5c1376e46ce0b1ec678bb892383478f
ShefAnn <167525730+ShefAnn@users.noreply.github.com>
2026-02-11 20:27:30 +0100

    Add files via upload

---
+++ RETFOUND/retfoun_ft_and_ev/ev_0_4_RF_mao_MES_val_BD_mini_NOISE_SMALL.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ==== Model settings ====
+# adaptation {finetune,lp}
+ADAPTATION="finetune"
+MODEL="RETFound_mae"
+MODEL_ARCH="retfound_mae"
+#FINETUNE="RETFound_mae_natureCFP"
+
+for FOLD in {0..9}
+do
+	# ==== Data settings ====
+	# # change the dataset name and corresponding class number
+	DATASET_TRAIN="MES_FOLD_04_${FOLD}"
+    DATASET="MES_val_BD_mini_FOLD_${FOLD}"
+	NUM_CLASS=5
+	# =======================
+    DATA_PATH="mini_BD_tvt_04/fold${FOLD}"
+    TASK_TRAIN="${MODEL_ARCH}_${DATASET_TRAIN}_${ADAPTATION}"
+    TASK="${MODEL_ARCH}_${DATASET}_${ADAPTATION}"
+    
+    # Path to the trained checkpoint (adjust if you saved elsewhere)
+    CKPT="./output_dir/${TASK_TRAIN}/checkpoint-best.pth"
+    # ==== Evaluation only ====
+    torchrun --nproc_per_node=1 --master_port=48766 extract_f_p_RETFound_noise.py \
+    --model "${MODEL}" \
+    --batch_size 128 \
+    --nb_classes "${NUM_CLASS}" \
+    --data_path "${DATA_PATH}" \
+    --input_size 224 \
+    --task "${TASK}" \
+    --num_workers 0 \
+    --resume "${CKPT}"\
+    --noise "small"        
+done 
---
+++ RETFOUND/retfoun_ft_and_ev/ft_ev_0_1_RF_mao_mini.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ==== Model settings ====
+# adaptation {finetune,lp}
+ADAPTATION="finetune"
+MODEL="RETFound_mae"
+MODEL_ARCH="retfound_mae"
+FINETUNE="RETFound_mae_natureCFP"
+
+for FOLD in {0..9}
+do
+	# ==== Data settings ====
+	# # change the dataset name and corresponding class number
+	DATASET="BD_mini_01_FOLD_${FOLD}"
+	NUM_CLASS=2
+	# =======================
+	DATA_PATH="mini_BD_tvt_01/fold${FOLD}"
+	TASK="${MODEL_ARCH}_${DATASET}_${ADAPTATION}"
+	torchrun --nproc_per_node=1 --master_port=48766 main_finetune.py \
+		--model "${MODEL}" \
+		--model_arch "${MODEL_ARCH}" \
+		--finetune "${FINETUNE}" \
+		--savemodel \
+		--global_pool \
+		--batch_size 24 \
+		--world_size 1 \
+		--epochs 50 \
+		--nb_classes "${NUM_CLASS}" \
+		--data_path "${DATA_PATH}" \
+		--input_size 224 \
+		--task "${TASK}" \
+		--adaptation "${ADAPTATION}" \
+        --num_workers 2 
+
+    sleep 20
+done 
